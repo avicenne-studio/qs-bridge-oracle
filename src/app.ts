36ca2109a2eed609f8b0c743347505c5137649e3
Jean <jean.antoine.michelet@gmail.com>
2025-11-25 16:47:36 +0100

    test: remove fastify-cli helper

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,27 @@
-import path from 'node:path'
-import fastifyAutoload from '@fastify/autoload'
-import { FastifyError, FastifyInstance, FastifyPluginOptions } from 'fastify'
+import path from "node:path";
+import fastifyAutoload from "@fastify/autoload";
+import { FastifyError, FastifyInstance, FastifyPluginOptions } from "fastify";
 
 export const options = {
   ajv: {
     customOptions: {
-      coerceTypes: 'array',
-      removeAdditional: 'all'
-    }
-  }
-}
+      coerceTypes: "array",
+      removeAdditional: "all",
+    },
+  },
+};
 
-export default async function serviceApp (
+export default async function serviceApp(
   fastify: FastifyInstance,
   opts: FastifyPluginOptions
 ) {
-  delete opts.skipOverride // This option only serves testing purpose
+  delete opts.skipOverride; // This option only serves testing purpose
   // This loads all external plugins defined in plugins/external
   // those should be registered first as your application plugins might depend on them
   await fastify.register(fastifyAutoload, {
-    dir: path.join(import.meta.dirname, 'plugins/external'),
-    options: {}
-  })
+    dir: path.join(import.meta.dirname, "plugins/external"),
+    options: {},
+  });
 
   // This loads all your application plugins defined in plugins/app
   // those should be support plugins that are reused
@@ -34,11 +34,11 @@ export default async function serviceApp (
   // This loads all plugins defined in routes
   // define your routes in one of these
   fastify.register(fastifyAutoload, {
-    dir: path.join(import.meta.dirname, 'routes'),
+    dir: path.join(import.meta.dirname, "routes"),
     autoHooks: true,
     cascadeHooks: true,
-    options: { ...opts }
-  })
+    options: { ...opts },
+  });
 
   fastify.setErrorHandler((err: FastifyError, request, reply) => {
     fastify.log.error(
@@ -48,29 +48,29 @@ export default async function serviceApp (
           method: request.method,
           url: request.url,
           query: request.query,
-          params: request.params
-        }
+          params: request.params,
+        },
       },
-      'Unhandled error occurred'
-    )
+      "Unhandled error occurred"
+    );
 
-    reply.code(err.statusCode ?? 500)
+    reply.code(err.statusCode ?? 500);
 
-    let message = 'Internal Server Error'
+    let message = "Internal Server Error";
     if (err.statusCode && err.statusCode < 500) {
-      message = err.message
+      message = err.message;
     }
 
-    return { message }
-  })
+    return { message };
+  });
 
   // An attacker could search for valid URLs if your 404 error handling is not rate limited.
   fastify.setNotFoundHandler(
     {
       preHandler: fastify.rateLimit({
         max: 3,
-        timeWindow: 500
-      })
+        timeWindow: 500,
+      }),
     },
     (request, reply) => {
       request.log.warn(
@@ -79,14 +79,15 @@ export default async function serviceApp (
             method: request.method,
             url: request.url,
             query: request.query,
-            params: request.params
-          }
+            params: request.params,
+          },
         },
-        'Resource not found'
-      )
+        "Resource not found"
+      );
 
-      reply.code(404)
+      reply.code(404);
 
-      return { message: 'Not Found' }
-    })
+      return { message: "Not Found" };
+    }
+  );
 }
--- test/helper.ts
+++ test/helper.ts
@@ -1,48 +1,38 @@
-import { FastifyInstance, LightMyRequestResponse } from 'fastify'
-import { build as buildApplication } from 'fastify-cli/helper.js'
-import path from 'node:path'
-import { TestContext } from 'node:test'
-import { options as serverOptions } from '../src/app.js'
-import assert from 'node:assert'
-
-const AppPath = path.join(import.meta.dirname, '../src/app.ts')
+import fastify, { LightMyRequestResponse } from "fastify";
+import { TestContext } from "node:test";
+import serviceApp, { options as serverOptions } from "../src/app.js";
+import assert from "node:assert";
+import fp from 'fastify-plugin'
 
 // Fill in this config with all the configurations
 // needed for testing the application
-export function config () {
+export function config() {
   return {
-    skipOverride: true // Register our application with fastify-plugin
-  }
+    skipOverride: true, // Register our application with fastify-plugin
+  };
 }
 
-export function expectValidationError (res: LightMyRequestResponse, expectedMessage: string) {
-  assert.strictEqual(res.statusCode, 400)
-  const { message } = JSON.parse(res.payload)
-  assert.strictEqual(message, expectedMessage)
+export function expectValidationError(
+  res: LightMyRequestResponse,
+  expectedMessage: string
+) {
+  assert.strictEqual(res.statusCode, 400);
+  const { message } = JSON.parse(res.payload);
+  assert.strictEqual(message, expectedMessage);
 }
 
 // automatically build and tear down our instance
-export async function build (t?: TestContext) {
+export async function build(t?: TestContext) {
   // you can set all the options supported by the fastify CLI command
-  const argv = [AppPath]
-
-  console.log('argv', argv)
-
-  // fastify-plugin ensures that all decorators
-  // are exposed for testing purposes, this is
-  // different from the production setup
-  const app = (await buildApplication(
-    argv,
-    config(),
-    serverOptions
-  )) as FastifyInstance
+  const app = fastify();
+  app.register(fp(serviceApp));
 
   // This is after start, so we can't decorate the instance using `.decorate`
 
   // If we pass the test contest, it will close the app after we are done
   if (t) {
-    t.after(() => app.close())
+    t.after(() => app.close());
   }
 
-  return app
+  return app;
 }
